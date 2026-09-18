fab2b8c207d595b246b72e57b43e01918be98ada
kessya wdanmylla <kessyawdanmylla@gmail.com>
2026-09-18 10:50:43 -0300

    atualizando projeto

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-       '@': path.resolve(import.meta.dirname, 'src'),
+        '@': path.resolve(__dirname, '.'),
       },
     },
     server: {
